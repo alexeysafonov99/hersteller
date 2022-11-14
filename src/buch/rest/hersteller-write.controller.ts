9a65615dd4e47fd7af9921773c0a4a489f06063f
Alexey Safonov <alexeyysafonov@gmail.com>
2022-11-14 23:30:07 +0100

    first commit

---
+++ src/buch/rest/hersteller-write.controller.ts
@@ -0,0 +1,349 @@
+/* eslint-disable max-lines */
+/*
+ * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Das Modul besteht aus der Controller-Klasse für Schreiben an der REST-Schnittstelle.
+ * @packageDocumentation
+ */
+
+import {
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiCreatedResponse,
+    ApiHeader,
+    ApiNoContentResponse,
+    ApiOperation,
+    ApiPreconditionFailedResponse,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
+import {
+    Body,
+    Controller,
+    Delete,
+    Headers,
+    HttpStatus,
+    Param,
+    Post,
+    Put,
+    Req,
+    Res,
+    UseGuards,
+    UseInterceptors,
+} from '@nestjs/common';
+import { type CreateError, type UpdateError } from '../service/errors.js';
+import { Request, Response } from 'express';
+import { type Hersteller } from '../entity/hersteller.entity.js';
+import { HerstellerWriteService } from '../service/Hersteller-write.service.js';
+import { JwtAuthGuard } from '../../security/auth/jwt/jwt-auth.guard.js';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { Roles } from '../../security/auth/roles/roles.decorator.js';
+import { RolesGuard } from '../../security/auth/roles/roles.guard.js';
+import { getBaseUri } from './getBaseUri.js';
+import { getLogger } from '../../logger/logger.js';
+
+export type HerstellerDTO = Omit<
+    Hersteller,
+    'aktualisiert' | 'erzeugt' | 'id' | 'version'
+>;
+
+export type HerstellerUpdateDTO = Omit<
+    Hersteller,
+    'aktualisiert' | 'erzeugt' | 'id' | 'version'
+>;
+
+/**
+ * Die Controller-Klasse für die Verwaltung von Herstellers.
+ */
+@Controller()
+@UseGuards(JwtAuthGuard, RolesGuard)
+@UseInterceptors(ResponseTimeInterceptor)
+@ApiTags('Hersteller API')
+@ApiBearerAuth()
+export class HerstellerWriteController {
+    readonly #service: HerstellerWriteService;
+
+    readonly #logger = getLogger(HerstellerWriteController.name);
+
+    constructor(service: HerstellerWriteService) {
+        this.#service = service;
+    }
+
+    /**
+     * Ein neuer Hersteller wird asynchron angelegt. Das neu anzulegende Hersteller ist als
+     * JSON-Datensatz im Request-Objekt enthalten. Wenn es keine
+     * Verletzungen von Constraints gibt, wird der Statuscode `201` (`Created`)
+     * gesetzt und im Response-Header wird `Location` auf die URI so gesetzt,
+     * dass damit das neu angelegte Hersteller abgerufen werden kann.
+     *
+     * Falls Constraints verletzt sind, wird der Statuscode `400` (`Bad Request`)
+     * gesetzt und genauso auch wenn der Name oder die ISBN-Nummer bereits
+     * existieren.
+     *
+     * @param Hersteller JSON-Daten für ein Hersteller im Request-Body.
+     * @param res Leeres Response-Objekt von Express.
+     * @returns Leeres Promise-Objekt.
+     */
+    @Post()
+    @Roles('admin', 'mitarbeiter')
+    @ApiOperation({ summary: 'Einen neuen Hersteller anlegen' })
+    @ApiCreatedResponse({ description: 'Erfolgreich neu angelegt' })
+    @ApiBadRequestResponse({ description: 'Fehlerhafte Herstellerdaten' })
+    async create(
+        @Body() herstellerDTO: HerstellerDTO,
+        @Req() req: Request,
+        @Res() res: Response,
+    ): Promise<Response> {
+        this.#logger.debug('create: herstellerDTO=%o', herstellerDTO);
+
+        const result = await this.#service.create(this.#dtoToHersteller(herstellerDTO));
+        if (Object.prototype.hasOwnProperty.call(result, 'type')) {
+            return this.#handleCreateError(result as CreateError, res);
+        }
+
+        const location = `${getBaseUri(req)}/${result as string}`;
+        this.#logger.debug('create: location=%s', location);
+        return res.location(location).send();
+    }
+
+    /**
+     * Ein vorhandener Hersteller wird asynchron aktualisiert.
+     *
+     * Im Request-Objekt von Express muss die ID des zu aktualisierenden Herstelleres
+     * als Pfad-Parameter enthalten sein. Außerdem muss im Rumpf das zu
+     * aktualisierende Hersteller als JSON-Datensatz enthalten sein. Damit die
+     * Aktualisierung überhaupt durchgeführt werden kann, muss im Header
+     * `If-Match` auf die korrekte Version für optimistische Synchronisation
+     * gesetzt sein.
+     *
+     * Bei erfolgreicher Aktualisierung wird der Statuscode `204` (`No Content`)
+     * gesetzt und im Header auch `ETag` mit der neuen Version mitgeliefert.
+     *
+     * Falls die Versionsnummer fehlt, wird der Statuscode `428` (`Precondition
+     * required`) gesetzt; und falls sie nicht korrekt ist, der Statuscode `412`
+     * (`Precondition failed`). Falls Constraints verletzt sind, wird der
+     * Statuscode `400` (`Bad Request`) gesetzt und genauso auch wenn der neue
+     * Name oder die neue ISBN-Nummer bereits existieren.
+     *
+     * @param hersteller Herstellerdaten im Body des Request-Objekts.
+     * @param id Pfad-Paramater für die ID.
+     * @param version Versionsnummer aus dem Header _If-Match_.
+     * @param res Leeres Response-Objekt von Express.
+     * @returns Leeres Promise-Objekt.
+     */
+    // eslint-disable-next-line max-params
+    @Put(':id')
+    @Roles('admin', 'mitarbeiter')
+    @ApiOperation({
+        summary: 'Ein vorhandenes Hersteller aktualisieren',
+        tags: ['Aktualisieren'],
+    })
+    @ApiHeader({
+        name: 'If-Match',
+        description: 'Header für optimistische Synchronisation',
+        required: false,
+    })
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'Header für JWT',
+        required: true,
+    })
+    @ApiNoContentResponse({ description: 'Erfolgreich aktualisiert' })
+    @ApiBadRequestResponse({ description: 'Fehlerhafte Herstellerdaten' })
+    @ApiPreconditionFailedResponse({
+        description: 'Falsche Version im Header "If-Match"',
+    })
+    @ApiResponse({
+        status: HttpStatus.PRECONDITION_REQUIRED,
+        description: 'Header "If-Match" fehlt',
+    })
+    async update(
+        @Body() herstellerDTO: HerstellerUpdateDTO,
+        @Param('id') id: string,
+        @Headers('If-Match') version: string | undefined,
+        @Res() res: Response,
+    ): Promise<Response> {
+        this.#logger.debug(
+            'update: id=%s, herstellerDTO=%o, version=%s',
+            id,
+            herstellerDTO,
+            version,
+        );
+
+        if (version === undefined) {
+            const msg = 'Header "If-Match" fehlt';
+            this.#logger.debug('#handleUpdateError: msg=%s', msg);
+            return res
+                .status(HttpStatus.PRECONDITION_REQUIRED)
+                .set('Content-Type', 'text/plain')
+                .send(msg);
+        }
+
+        const result = await this.#service.update(
+            id,
+            this.#updateDtoToHersteller(herstellerDTO),
+            version,
+        );
+        if (typeof result === 'object') {
+            return this.#handleUpdateError(result, res);
+        }
+
+        this.#logger.debug('update: version=%d', result);
+        return res.set('ETag', `"${result}"`).sendStatus(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Ein Hersteller wird anhand seiner ID-gelöscht, die als Pfad-Parameter angegeben
+     * ist. Der zurückgelieferte Statuscode ist `204` (`No Content`).
+     *
+     * @param id Pfad-Paramater für die ID.
+     * @param res Leeres Response-Objekt von Express.
+     * @returns Leeres Promise-Objekt.
+     */
+    @Delete(':id')
+    @Roles('admin')
+    @ApiOperation({ summary: 'Hersteller mit der ID löschen', tags: ['Loeschen'] })
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'Header für JWT',
+        required: true,
+    })
+    @ApiNoContentResponse({
+        description: 'Das Hersteller wurde gelöscht oder war nicht vorhanden',
+    })
+    async delete(
+        @Param('id') id: string,
+        @Res() res: Response,
+    ): Promise<Response<undefined>> {
+        this.#logger.debug('delete: id=%s', id);
+
+        try {
+            await this.#service.delete(id);
+        } catch (err) {
+            this.#logger.error('delete: error=%o', err);
+            return res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return res.sendStatus(HttpStatus.NO_CONTENT);
+    }
+
+    #dtoToHersteller(herstellerDTO: HerstellerDTO): Hersteller {
+        const hersteller: Hersteller = {
+            id: undefined,
+            version: undefined,
+            name: herstellerDTO.name,
+            telefon: herstellerDTO.telefon,
+            homepage: herstellerDTO.homepage,
+            erzeugt: undefined,
+            aktualisiert: undefined,
+        };
+
+        return hersteller;
+    }
+
+    #handleCreateError(err: CreateError, res: Response) {
+        switch (err.type) {
+            case 'ConstraintViolations':
+                return this.#handleValidationError(err.messages, res);
+
+            case 'NameExists':
+                return this.#handleNameExists(err.name, res);
+
+            default:
+                return res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    #handleValidationError(
+        messages: readonly string[],
+        res: Response,
+    ): Response {
+        this.#logger.debug('#handleValidationError: messages=%o', messages);
+        return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send(messages);
+    }
+
+    #handleNameExists(
+        name: string | null | undefined,
+        res: Response,
+    ): Response {
+        const msg = `Der Name "${name}" existiert bereits.`;
+        this.#logger.debug('#handleNExists(): msg=%s', msg);
+        return res
+            .status(HttpStatus.UNPROCESSABLE_ENTITY)
+            .set('Content-Type', 'text/plain')
+            .send(msg);
+    }
+
+    #updateDtoToHersteller(herstellerDTO: HerstellerUpdateDTO): Hersteller {
+        const hersteller: Hersteller = {
+            id: undefined,
+            version: undefined,
+            name: herstellerDTO.name,
+            telefon: herstellerDTO.telefon,
+            homepage: herstellerDTO.homepage,
+            erzeugt: undefined,
+            aktualisiert: undefined,
+        };
+
+        return hersteller;
+    }
+
+    #handleUpdateError(err: UpdateError, res: Response): Response {
+        switch (err.type) {
+            case 'ConstraintViolations':
+                return this.#handleValidationError(err.messages, res);
+
+            case 'HerstellerNotExists': {
+                const { id } = err;
+                const msg = `Es gibt kein Hersteller mit der ID "${id}".`;
+                this.#logger.debug('#handleUpdateError: msg=%s', msg);
+                return res
+                    .status(HttpStatus.PRECONDITION_FAILED)
+                    .set('Content-Type', 'text/plain')
+                    .send(msg);
+            }
+
+            case 'NameExists':
+                return this.#handleNameExists(err.name, res);
+
+            case 'VersionInvalid': {
+                const { version } = err;
+                const msg = `Die Versionsnummer "${version}" ist ungueltig.`;
+                this.#logger.debug('#handleUpdateError: msg=%s', msg);
+                return res
+                    .status(HttpStatus.PRECONDITION_FAILED)
+                    .set('Content-Type', 'text/plain')
+                    .send(msg);
+            }
+
+            case 'VersionOutdated': {
+                const { version } = err;
+                const msg = `Die Versionsnummer "${version}" ist nicht aktuell.`;
+                this.#logger.debug('#handleUpdateError: msg=%s', msg);
+                return res
+                    .status(HttpStatus.PRECONDITION_FAILED)
+                    .set('Content-Type', 'text/plain')
+                    .send(msg);
+            }
+
+            default:
+                return res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+/* eslint-enable max-lines */
---
+++ src/buch/service/hersteller-read.service.ts
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Das Modul besteht aus der Klasse {@linkcode HerstellerReadService}.
+ * @packageDocumentation
+ */
+
+import { Hersteller} from '../entity/hersteller.entity.js';
+import { HerstellerValidationService } from './buch-validation.service.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { QueryBuilder } from './query-builder.js';
+import { Repository } from 'typeorm';
+import { getLogger } from '../../logger/logger.js';
+
+export interface Suchkriterien {
+    readonly name?: string;
+    readonly telefon?: string;
+    readonly homepage?: string;
+    readonly javascript?: boolean;
+    readonly typescript?: boolean;
+}
+
+/**
+ * Die Klasse `HerstellerReadService` implementiert das Lesen für Bücher und greift
+ * mit _TypeORM_ auf eine relationale DB zu.
+ */
+@Injectable()
+export class HerstellerReadService {
+    readonly #repo: Repository<Hersteller>;
+
+    readonly #herstellerProps: string[];
+
+    readonly #queryBuilder: QueryBuilder;
+
+    readonly #validationService: HerstellerValidationService;
+
+    readonly #logger = getLogger(HerstellerReadService.name);
+
+    constructor(
+        @InjectRepository(Hersteller) repo: Repository<Hersteller>,
+        queryBuilder: QueryBuilder,
+        validationService: HerstellerValidationService,
+    ) {
+        this.#repo = repo;
+        const herstellerDummy = new Hersteller();
+        this.#herstellerProps = Object.getOwnPropertyNames(herstellerDummy);
+        this.#queryBuilder = queryBuilder;
+        this.#validationService = validationService;
+    }
+
+    // Rueckgabetyp Promise bei asynchronen Funktionen
+    //    ab ES2015
+    //    vergleiche Task<> bei C# und Mono<> aus Project Reactor
+    // Status eines Promise:
+    //    Pending: das Resultat ist noch nicht vorhanden, weil die asynchrone
+    //             Operation noch nicht abgeschlossen ist
+    //    Fulfilled: die asynchrone Operation ist abgeschlossen und
+    //               das Promise-Objekt hat einen Wert
+    //    Rejected: die asynchrone Operation ist fehlgeschlagen and das
+    //              Promise-Objekt wird nicht den Status "fulfilled" erreichen.
+    //              Im Promise-Objekt ist dann die Fehlerursache enthalten.
+
+    /**
+     * Ein Hersteller asynchron anhand seiner ID suchen
+     * @param id ID des gesuchten Herstelleres
+     * @returns Das gefundene Hersteller vom Typ [Hersteller](Hersteller_entity_Hersteller_entity.Hersteller.html) oder undefined
+     *          in einem Promise aus ES2015 (vgl.: Mono aus Project Reactor oder
+     *          Future aus Java)
+     */
+    async findById(id: string) {
+        this.#logger.debug('findById: id=%s', id);
+
+        if (!this.#validationService.validateId(id)) {
+            this.#logger.debug('findById: Ungueltige ID');
+            return;
+        }
+
+        // https://typeorm.io/working-with-repository
+        // Das Resultat ist undefined, falls kein Datensatz gefunden
+        // Lesen: Keine Transaktion erforderlich
+        const hersteller = await this.#queryBuilder.buildId(id).getOne();
+        if (hersteller === null) {
+            this.#logger.debug('findById: Kein Hersteller gefunden');
+            return;
+        }
+
+        this.#logger.debug('findById: hersteller=%o', hersteller);
+        return hersteller;
+    }
+
+    /**
+     * Bücher asynchron suchen.
+     * @param suchkriterien JSON-Objekt mit Suchkriterien
+     * @returns Ein JSON-Array mit den gefundenen Büchern. Ggf. ist das Array leer.
+     */
+    async find(suchkriterien?: Suchkriterien) {
+        this.#logger.debug('find: suchkriterien=%o', suchkriterien);
+
+        // Keine Suchkriterien?
+        if (suchkriterien === undefined) {
+            return this.#findAll();
+        }
+        const keys = Object.keys(suchkriterien);
+        if (keys.length === 0) {
+            return this.#findAll();
+        }
+
+        // Falsche Namen fuer Suchkriterien?
+        if (!this.#checkKeys(keys)) {
+            return [];
+        }
+
+        // QueryBuilder https://typeorm.io/select-query-builder
+        // Das Resultat ist eine leere Liste, falls nichts gefunden
+        // Lesen: Keine Transaktion erforderlich
+        const herstellers = await this.#queryBuilder.build(suchkriterien).getMany();
+        this.#logger.debug('find: herstellers=%o', herstellers);
+
+        return herstellers;
+    }
+
+    async #findAll() {
+        const herstellers = await this.#repo.find();
+        this.#logger.debug('#findAll: alle herstellers=%o', herstellers);
+        return herstellers;
+    }
+
+    #checkKeys(keys: string[]) {
+        // Ist jedes Suchkriterium auch eine Property von Hersteller oder "schlagwoerter"?
+        let validKeys = true;
+        keys.forEach((key) => {
+            if (
+                !this.#herstellerProps.includes(key) &&
+                key !== 'javascript' &&
+                key !== 'typescript'
+            ) {
+                this.#logger.debug(
+                    '#find: ungueltiges Suchkriterium "%s"',
+                    key,
+                );
+                validKeys = false;
+            }
+        });
+
+        return validKeys;
+    }
+}
